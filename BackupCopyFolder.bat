99442250d4b73f434980bcfb60b12e10a5116bac
Patrick Seiter <patrick.seiter@gmail.com>
2016-09-26 02:18:29 -0500

    Add Windows 10-specific flags

--- BackupCopyFolder.bat
+++ BackupCopyFolder.bat
@@ -1,2 +1,7 @@
-ROBOCOPY "%~1" "%~2%~3" /E /COPYALL /DCOPY:T /SL
+FOR /f "tokens=4-5 delims=. " %%i IN ('ver') DO SET VERSION=%%i.%%j
+if "%version%" == "10.0" (
+	ROBOCOPY "%~1" "%~2%~3" /E /COPYALL /DCOPY:DAT /SL
+) ELSE (
+	ROBOCOPY "%~1" "%~2%~3" /E /COPYALL /DCOPY:T /SL
+)
 REM /XC /XN /XO /COPY:DAT
--- SnapshotCopyFolder.bat
+++ SnapshotCopyFolder.bat
@@ -1,8 +1,13 @@
 NET USE \\%4\%~2 /u:%4\%5 %6 >nul 2>&1
 IF "%ERRORLEVEL%"=="0" (
-	REM ECHO ROBOCOPY "%~1" "\\%4\%~2\SNAPSHOT\%~3" /E /XD "*Cache*" /COPYALL /PURGE /DCOPY:T /SL /XJD
-	ROBOCOPY "%~1" "\\%4\%~2\SNAPSHOT\%~3" /E /XD "*Cache*" /COPYALL /PURGE /DCOPY:T /SL /XJD
-	REM /XC /XN /XO /COPY:DAT
+	FOR /f "tokens=4-5 delims=. " %%i IN ('ver') DO SET VERSION=%%i.%%j
+	if "%version%" == "10.0" (
+		ROBOCOPY "%~1" "\\%4\%~2\SNAPSHOT\%~3" /E /XD "*Cache*" /COPYALL /PURGE /DCOPY:DAT /SL /XJD /UNICODE
+	) ELSE (
+		REM ECHO ROBOCOPY "%~1" "\\%4\%~2\SNAPSHOT\%~3" /E /XD "*Cache*" /COPYALL /PURGE /DCOPY:T /SL /XJD
+		ROBOCOPY "%~1" "\\%4\%~2\SNAPSHOT\%~3" /E /XD "*Cache*" /COPYALL /PURGE /DCOPY:T /SL /XJD /UNICODE
+		REM /XC /XN /XO /COPY:DAT
+	)
 ) ELSE (
 	ECHO "SnapshotCopyFolder.bat: Could not connect to %4!"
 )
